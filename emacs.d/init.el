a13c327639c82a06f226fd06d029a8a2f0cc004f
huangchao <huangchao.cpp@gmail.com>
2016-04-30 23:57:21 +0800

    add mysql port

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -43,3 +43,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'sql)
+(setq sql-mysql-login-params (append sql-mysql-login-params '(port)))
