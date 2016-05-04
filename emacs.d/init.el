c11a06739a92aa44de2e685aa0af24dc11cf5d85
HuangChao <huangchao.cpp@gmail.com>
2016-05-04 23:58:41 +0800

    add theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,20 +20,8 @@
 (require 'ace-jump-mode)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 (require 'magit)
-
+(require 'init-themes)
 (provide 'init)
-
-;; initial window settings
-(setq initial-frame-alist
-      '((background-color . "black")))
-
-;; subsequent window settings
-(setq default-frame-alist
-      '((background-color . "black")))
-
-(load-theme 'wombat t)
-
-
 
 ;; Save all tempfiles in $TMPDIR/emacs$UID/                                                        
 ;;(defconst emacs-tmp-dir (format "%s/%s/" temporary-file-directory  "emacs" ))
@@ -49,7 +37,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/git-local/chaoswork/work-records/daily-work/workspace/work.org"))))
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(org-agenda-files
+   (quote
+    ("~/git-local/chaoswork/work-records/daily-work/workspace/work.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
