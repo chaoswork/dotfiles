0e0c0606ef84f042715eb1d25215b64611227940
huangchao <huangchao.cpp@gmail.com>
2016-05-05 11:14:52 +0800

    change theme to monokai

--- emacs.d/lisp/init-themes.el
+++ emacs.d/lisp/init-themes.el
@@ -2,7 +2,7 @@
 (require-package 'color-theme-sanityinc-tomorrow)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-solarized-light))
+(setq-default custom-enabled-themes '(monokai))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
@@ -18,15 +18,15 @@
 ;;------------------------------------------------------------------------------
 ;; Toggle between light and dark
 ;;------------------------------------------------------------------------------
-(defun light ()
-  "Activate a light color theme."
-  (interactive)
-  (color-theme-sanityinc-solarized-light))
-
-(defun dark ()
-  "Activate a dark color theme."
-  (interactive)
-  (color-theme-sanityinc-solarized-dark))
-
+;;(defun light ()
+;;  "Activate a light color theme."
+;;  (interactive)
+;;  (color-theme-sanityinc-solarized-light))
+;;
+;;(defun dark ()
+;;  "Activate a dark color theme."
+;;  (interactive)
+;;  (color-theme-sanityinc-solarized-dark))
+;;
 
 (provide 'init-themes)
